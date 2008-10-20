1f1b55eaa90df3ce0edcd7f91d2128371ea34a9f
adetayo_kasim <adetayo_kasim@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-20 10:28:48 +0000

    isominimax is an alternative function to calculate isotonic means.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/orcme@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/isominimax.r
@@ -0,0 +1,97 @@
+## monotonicity
+minmax <- function(tmpMatrix){
+  tmp<-rep(NA,ncol(tmpMatrix))
+  for(i in 1:ncol(tmpMatrix)){
+   tmp[i] <- max(tmpMatrix[,i])
+  }
+  return(min(tmp))
+}
+
+
+isominmax <- function(mean.y,invsigma,isoDir){
+k <- length(mean.y)
+mu.data <- rep(NA,k)
+
+if(isoDir == "up"){
+  for(i in 1:k){
+    L <- c(1:i)
+    U <- c(i:k)
+    tmpmat <- matrix(NA,nrow = length(L),ncol=length(U))
+    for(j in 1:length(L)) {
+      for(l in 1:length(U)){
+
+        tmpidentity <- rep(1,(U[l]-L[j]+1))
+        tmpinvsigma <- invsigma[L[j]:U[l],L[j]:U[l]]
+        tmpmu <- mean.y[L[j]:U[l]]
+        tmpnumerator <-  t(tmpidentity) %*%  tmpinvsigma %*% tmpmu
+        tmpdenominator <- t(tmpidentity) %*%  tmpinvsigma %*% tmpidentity
+        tmpmat[j,l] <-   tmpnumerator/ tmpdenominator
+      }
+
+    }
+        mu.data[i] <- minmax(tmpMatrix=tmpmat)[1]
+  }
+ }else if(isoDir == "dn"){
+for(i in k:1){
+    L <- c(k:i)
+    U <- c(i:1)
+    tmpmat <- matrix(NA,nrow = length(L),ncol=length(U))
+    for(j in 1:length(L)) {
+      for(l in 1:length(U)){
+
+        tmpidentity <- rep(1,(L[j]-U[l]+1))
+        tmpinvsigma <- invsigma[L[j]:U[l],L[j]:U[l]]
+        tmpmu <- mean.y[L[j]:U[l]]
+        tmpnumerator <-  t(tmpidentity) %*%  tmpinvsigma %*% tmpmu
+        tmpdenominator <- t(tmpidentity) %*%  tmpinvsigma %*% tmpidentity
+        tmpmat[j,l] <-   tmpnumerator/ tmpdenominator
+      }
+
+    }
+        mu.data[i] <- minmax(tmpMatrix=tmpmat)[1]
+  }
+
+}
+  return( mu.data)
+}
+
+isominmax <- function(compData,doseData){
+  compData2 <- compData[,order(doseData,decreasing=F)]
+  doseData2 <- doseData[order(doseData,decreasing=F)]
+  isotmp <- rep(NA,nrow(compData2))
+  isomeans <- matrix(NA,nrow=nrow(compData2),ncol=length(unique(doseData2)))
+  for(i in 1:nrow(compData)) {
+    exampleminmax <-  compData2[i,]
+    mean.exampleminmax <- tapply(exampleminmax, doseData2, mean)
+    cvar.exampleminmax  <-  var(exampleminmax)
+    cinvvar.exampleminmax <- 1/cvar.exampleminmax
+    ndose <- tapply(doseData,doseData,length)
+    cinvvarmat.exampleminmax  <- diag(cinvvar.exampleminmax,nrow=length(mean.exampleminmax),ncol=length(mean.exampleminmax))
+    upcmean.minmax <- isominmax(mean.y=mean.exampleminmax,invsigma=cinvvarmat.exampleminmax,isoDir ="up")
+    downcmean.minmax <- isominmax(mean.y=mean.exampleminmax,invsigma=cinvvarmat.exampleminmax,isoDir ="dn")
+
+    likelihoodmu.dose <-  rep(mean.exampleminmax[1],ndose[1])
+    upmu.dose <-  rep(upcmean.minmax[1],ndose[1])
+    downmu.dose <-  rep(downcmean.minmax[1],ndose[1])
+    for(i in 2:length(mean.exampleminmax)){
+      likelihoodmu.dose <- c(likelihoodmu.dose,rep(mean.exampleminmax[i],ndose[i]))
+      upmu.dose <-  c(upmu.dose,rep(upcmean.minmax[i],ndose[i]))
+      downmu.dose <-  c(downmu.dose,rep(downcmean.minmax[i],ndose[i]))
+    }
+
+    nullvar <- sum((exampleminmax - likelihoodmu.dose)^2)
+    upvar <- sum((exampleminmax -  upmu.dose)^2)
+    dnvar <- sum((exampleminmax - downmu.dose)^2)
+    uplike <- (nullvar-upvar)/nullvar
+    dnlike <- (nullvar-dnvar)/nullvar
+    isotmp[i] <- c("up","dn")[ c(uplike,dnlike) == max(c(uplike,dnlike))]
+    if(isotmp[i]=="up"){
+      isomeans[i,]  <-  upcmean.minmax
+    } else{
+      isomeans[i,]  <-  downcmean.minmax
+    }
+  }
+    outtmp <- data.frame(Direction = isotmp, isomeans )
+    names(outtmp) <-c("Direction",unique(doseData2))
+    return(outtmp)
+}
