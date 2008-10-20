bad516ce1e2d0a8418949042f89485527bc4868b
adetayo_kasim <adetayo_kasim@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-20 14:51:15 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/orcme@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/logicalMask.R
+++ pkg/R/logicalMask.R
@@ -1,12 +1,6 @@
-<<<<<<< .mine
-`logicalMask` <- function(Cdata,data){
-  rprow <- as.vector(row.names(data))
-  rpcdata <- as.vector(row.names(Cdata))
-=======
 `logicalMask` <- function(Cdata, data){
   rprow <- row.names(data)
   rpcdata <- row.names(Cdata)
->>>>>>> .r16
   rpsel <- rprow %in% rpcdata  
   return(rpsel)
 }
