19d954af4a0761e75a4ec943f0633e89607e4c53
adso <adso@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-16 14:41:10 +0000

    Robust clustering based on median polish added.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/orcme@56 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ORCME.R
+++ pkg/R/ORCME.R
@@ -1,19 +1,34 @@
-`addRows` <- function(originalData, clusteredData) {
+`addRows` <- function(originalData, clusteredData, robust) {
 
-  HCluster <- meanSquaredResidue(clusteredData)$`overallHvalue `
-  
-  overallMeanCluster <- sum(clusteredData) / (nrow(clusteredData) * ncol(clusteredData))
-  doseMeansCluster <- colSums(clusteredData) / nrow(clusteredData)
-  newGenesMeans <- rowSums(originalData) / ncol(originalData)
-  list <- seq(1:nrow(originalData))
-  ResidualsData <- unlist(lapply(list, function (x) mean(((originalData[x,] - newGenesMeans[x]) - (doseMeansCluster - overallMeanCluster))^2)))
-  
+  HCluster <- Hcriterion(clusteredData, robust=robust)$`overallHvalue `
+
+  if(robust==FALSE){
+	  overallMeanCluster <- sum(clusteredData) / (nrow(clusteredData) * ncol(clusteredData))
+	  doseMeansCluster <- colSums(clusteredData) / nrow(clusteredData)
+	  newGenesMeans <- rowSums(originalData) / ncol(originalData)
+	  list <- seq(1:nrow(originalData))
+	  ResidualsData <- unlist(lapply(list, function (x) mean(((originalData[x,] - newGenesMeans[x]) - (doseMeansCluster - overallMeanCluster))^2)))
+
+  } 
+  if(robust==TRUE){
+	  mp <- medpolish(clusteredData,trace.iter=FALSE)
+	  overallMeanCluster <- mp$overall
+	  doseMeansCluster <- mp$col
+	  newGenesMeans <- medpolish(originalData,trace.iter=FALSE)$row
+	  list <- seq(1:nrow(originalData))
+	  ResidualsData <- unlist(lapply(list, function (x) mean(as.numeric(abs((originalData[x,] - newGenesMeans[x]) - (doseMeansCluster - overallMeanCluster))))))
+  }
+
   geneAddition <- ResidualsData < HCluster
   geneAddition <- geneAddition[!(rownames(originalData)[geneAddition] %in% row.names(clusteredData))] 
 
-  if(sum(geneAddition)!=0){
+    if(sum(geneAddition)!=0){
        addData <- originalData[geneAddition, ]
+       #clusteredData$ID <- rownames(clusteredData)
+       #addData$ID <- rownames(addData)
        clusteredOutput <-  rbind(clusteredData,addData)
+       #clusteredOutput <-  clusteredOutput[!duplicated(clusteredOutput$ID),]
+       #clusteredOutput <- clusteredOutput[,-5]
        dataLogic <-   row.names(originalData) %in% row.names(clusteredOutput)
        dataOutput <- originalData[!dataLogic, ]
   }else{
@@ -31,7 +46,7 @@
      return(idlogic)
 }
 
-`clusteredMem` <- function(parentData,incClusterData){
+`clusteredMem` <- function(parentData, incClusterData){
     parentData <- parentData
     ncluster <- c(1:length(incClusterData))
     pnames <- row.names(parentData)
@@ -40,38 +55,43 @@
 }
 
 
-`meanSquaredResidue` <- function(arrayData,addRow=FALSE){
+`Hcriterion` <- function(arrayData, addRow=FALSE, robust){
 
-  overallMean <- sum(arrayData) / ( nrow(arrayData) * ncol(arrayData) )
-  overallMeanMat <- matrix(overallMean , nrow = nrow(arrayData), ncol = ncol(arrayData))
+	if(robust==FALSE){
+  		overallMean <- sum(arrayData) / ( nrow(arrayData) * ncol(arrayData) )
+  		overallMeanMat <- matrix(overallMean , nrow = nrow(arrayData), ncol = ncol(arrayData))
 
-  geneMeans <- rowSums(arrayData) / ncol(arrayData)
-  geneMeansMat <- matrix(geneMeans, nrow = nrow(arrayData),ncol = ncol(arrayData))
+  		geneMeans <- rowSums(arrayData) / ncol(arrayData)
+  		geneMeansMat <- matrix(geneMeans, nrow = nrow(arrayData),ncol = ncol(arrayData))
 
-  doseMeans <- colSums(arrayData) / nrow(arrayData)
-  doseMeansMat <- t(matrix(t(doseMeans ), ncol = nrow(arrayData),nrow = ncol(arrayData)))
-  geneDoseResiduals <- (arrayData - geneMeansMat  - doseMeansMat
-  + overallMeanMat)^2
-
-  geneResiduals <- rowSums(geneDoseResiduals) / ncol(geneDoseResiduals)
-  overallHvalue <- sum(geneDoseResiduals ) / (nrow(geneDoseResiduals) * ncol(geneDoseResiduals))
-
+  		doseMeans <- colSums(arrayData) / nrow(arrayData)
+  		doseMeansMat <- t(matrix(t(doseMeans ), ncol = nrow(arrayData),nrow = ncol(arrayData)))
+  		geneDoseResiduals <- (arrayData - geneMeansMat  - doseMeansMat  + overallMeanMat)^2
+  		geneResiduals <- rowSums(geneDoseResiduals) / ncol(geneDoseResiduals)
+  		overallHvalue <- sum(geneDoseResiduals) / (nrow(geneDoseResiduals) * ncol(geneDoseResiduals))
+	} 
+	if(robust==TRUE){
+		geneDoseResiduals <- abs(medpolish(arrayData,trace.iter=FALSE)$residuals)
+		geneResiduals <- rowSums(geneDoseResiduals) / ncol(geneDoseResiduals)
+  		overallHvalue <- sum(geneDoseResiduals) / (nrow(geneDoseResiduals) * ncol(geneDoseResiduals))
+	}
+	
   mseData <- list(geneResiduals ,overallHvalue )
-  names(mseData ) <- c("geneResiduals ","overallHvalue ")
-  return(mseData )
+  names(mseData) <- c("geneResiduals ","overallHvalue ")
+  return(mseData)
 }
 
 
-`deltaInit` <- function(arrayData,lambda){
+`deltaInit` <- function(arrayData,lambda, robust){
  
-  initialHvalue <- meanSquaredResidue(arrayData)
+  initialHvalue <- Hcriterion(arrayData, robust=robust)
   delta <- lambda * initialHvalue$overallHvalue  
 
   return(delta)
 }
 
-`singleNodeDeletion` <- function(arrayData,delta,phi){
-  sndData <-meanSquaredResidue(arrayData)
+`singleNodeDeletion` <- function(arrayData,delta,phi, robust){
+  sndData <-Hcriterion(arrayData, robust=robust)
   overallHvalue <- sndData$overallHvalue
   geneResiduals <- sndData$geneResiduals
   fuse<-1
@@ -83,10 +103,11 @@
 
   while(overallHvalue > delta & dim(arrayData)[1]>phi & fuse>0)  {
       geneResidualsLogic <- geneResiduals < max(geneResiduals)
-      if(dim(arrayData[geneResidualsLogic , ])[1]==0){fuse<-0
+	  
+      if(dim(arrayData[geneResidualsLogic , ])[1]==0){fuse <- 0
 	} else{
-	 arrayData <- arrayData[geneResidualsLogic , ]
-       sndData <- meanSquaredResidue(arrayData)
+	   arrayData <- arrayData[geneResidualsLogic , ]
+       sndData <- Hcriterion(arrayData, robust=robust)
        overallHvalue <- sndData$overallHvalue
        geneResiduals <- sndData$geneResiduals
 	 }
@@ -98,21 +119,21 @@
 
 
 
-`ORCME` <- function(DRdata,lambda,phi){
+`ORCME` <- function(DRdata,lambda,phi, robust=FALSE){
   clusterRowNames <- list()
-  DRdata <- as.data.frame(DRdata )
+  DRdata <- as.data.frame(DRdata)
   row.names(DRdata) <- paste("g",c(1:dim(DRdata)[1]),sep="")
   DDdata <- DRdata
-  delta <- deltaInit(arrayData = DDdata, lambda)
+  delta <- deltaInit(arrayData = DDdata, lambda, robust=robust)
   phi <- phi
   iter <- 1
   while (dim(DDdata)[1] > phi){
 
-    ##apply sinlge node deletion algorithm of Cheng and Church,2000
-    clsData <- singleNodeDeletion(arrayData=DDdata,delta,phi)
+    ## apply single node deletion algorithm of Cheng and Church, 2000
+    clsData <- singleNodeDeletion(arrayData=DDdata, delta, phi, robust=robust)
 
-    ## apply algorithm for row addition by Cheng and Church,2000
-    clusterAddRows <- addRows(originalData = DDdata, clusteredData =clsData)
+    ## apply algorithm for row addition by Cheng and Church, 2000
+    clusterAddRows <- addRows(originalData = DDdata, clusteredData =clsData, robust=robust)
 
     ## discovered cluster
     clusterRowNames[[iter]] <- row.names(clusterAddRows$clusteredOutput)
@@ -125,4 +146,22 @@
   output <- as.data.frame(clusout[,colSums(clusout)!=0])
   row.names(output) <- row.names(DRdata)
   return(output)
+}
+
+
+EstimatedParameters <- function(DataSet, ORCMEoutput, clusterID, robust=FALSE){
+	useData <- DataSet[ORCMEoutput[,clusterID],]
+	if(robust==FALSE){
+  		overallMean <- sum(useData) / ( nrow(useData) * ncol(useData) )
+  		rowEffects <- rowSums(useData) / ncol(useData) - overallMean
+  		colEffects <- colSums(useData) / nrow(useData) - overallMean
+	} else {
+		mp <- medpolish(useData,trace.iter=FALSE)
+  		overallMean <- mp$overall
+  		rowEffects <- mp$row
+  		colEffects <- mp$col
+	}
+	result <- list(overallMean, rowEffects, colEffects)
+	names(result) <- c("overallMean", "rowEffects", "colEffects")
+return(result)
 }
--- pkg/R/resampleORCME.R
+++ pkg/R/resampleORCME.R
@@ -1,4 +1,4 @@
-clusterSumofSquares<- function(clusterOutput,sampledClusteringData){
+clusterSumofSquares<- function(clusterOutput, sampledClusteringData){
    wss <- NULL
   for(k in 1:length(clusterOutput)){
    clusteredDatak <-  as.matrix(clusterOutput[[k]])
@@ -24,9 +24,23 @@ SumofSquares<- function(arrayData){
 }
 
 
+clusterSumofAbsValues <- function(clusterOutput, sampledClusteringData){
+   wss <- NULL
+  for(k in 1:length(clusterOutput)){
+   clusteredDatak <-  as.matrix(clusterOutput[[k]])
+   wss[k] <- sum(apply(clusteredDatak,2,function(x) sum(rowSums(SumofAbsValues(arrayData=sampledClusteringData[x,])))))
+  }
+  return(wss)
+}
 
 
-resampleORCME <- function(clusteringData,lambdaVector){
+SumofAbsValues <- function(arrayData){
+geneDoseResiduals <- abs(medpolish(arrayData,trace.iter=FALSE)$residuals)
+return(abs(geneDoseResiduals^2))
+}
+
+
+resampleORCME <- function(clusteringData, lambdaVector, robust=FALSE){
   nGene=100
   nData=100
   iter <- 1
@@ -36,15 +50,20 @@ resampleORCME <- function(clusteringData,lambdaVector){
     ngene <- c(1:dim(clusteringData)[1])	
     sampledGenes <- sample(ngene,nGene,replace=TRUE)
     sampledClusteringData <- clusteringData[sampledGenes,]
-    clusterOutput <- sapply(lambdaVector,function(x)ORCME(DRdata=sampledClusteringData,lambda=x,phi=2))
-    TSS <-  sum(rowSums(SumofSquares(arrayData=sampledClusteringData)))
+    clusterOutput <- lapply(lambdaVector,function(x)ORCME(DRdata=sampledClusteringData,lambda=x,phi=2,robust=robust))
     nc <- sapply(c(1:length(clusterOutput)),function(x)dim(as.matrix(clusterOutput[[x]]))[2])
-    WSS <-  clusterSumofSquares(clusterOutput=clusterOutput,sampledClusteringData=sampledClusteringData)
+    
+    if (robust==FALSE){    
+	TSS <- sum(rowSums(SumofSquares(arrayData=sampledClusteringData)))
+    	WSS <- clusterSumofSquares(clusterOutput=clusterOutput, sampledClusteringData=sampledClusteringData)
+	} else {
+	TSS <- sum(rowSums(SumofAbsValues(arrayData=sampledClusteringData)))
+	WSS <- clusterSumofAbsValues(clusterOutput=clusterOutput, sampledClusteringData=sampledClusteringData)
+	}
+
     output[[iter]] <-  cbind(lambda=lambdaVector,WSS=WSS,TSS=TSS,nc=nc)
     iter <- iter+1
   }
   return(output)
 }
-
-
 
